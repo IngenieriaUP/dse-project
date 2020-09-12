c4c77cc081d0340d44e01cead7efd900facd266b
Claudio Ortega <claudio.rtega2701@gmail.com>
2020-09-12 00:35:29 -0500

    more formal app name

--- linioexp/settings.py
+++ linioexp/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'main',
+    'main.apps.MainConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
