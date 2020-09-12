08135b445e93d0f53462f6f3a1a5c2a40e11de5d
Claudio Ortega <claudio.rtega2701@gmail.com>
2020-09-12 00:36:33 -0500

    Use django generic views to visualize products in web app

--- main/views.py
+++ main/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
+
+from .models import *
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the main index.")
+
+class ProductListView(ListView):
+    model = Producto
+
+class ProductDetailView(DetailView):
+    model = Producto
