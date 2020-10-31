3ee8301b36a6c53644ec4af1257212aa8e46fd4e
Claudio <claudio.rtega2701@gmail.com>
2020-10-31 00:05:01 -0500

    prepare for heroku env

--- linioexp/settings.py
+++ linioexp/settings.py
@@ -140,3 +140,7 @@ EMAIL_PORT = '2525'
 
 DEFAULT_FILE_STORAGE = 'storages.backends.dropbox.DropBoxStorage'
 DROPBOX_OAUTH2_TOKEN = 'sl.Akm47PxxrmQ0d-NAR6MflrCTV8Uu-D-ToaX5JXgxBi_GtB7jbupHUkfaOV0byHnirrPwknkscGa-30S7DfYY3Y9eQQv_Qxb8UnLwov1RIsbQ2zNjXkJos_IyFO8P48z9pyU_CqQ'
+
+# Django Heroku
+import django_heroku
+django_heroku.settings(locals())
